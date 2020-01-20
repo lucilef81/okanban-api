2ef28139f2e11a2885862c79d7c8150d55d4c7ed
jpruliere <jean@oclock.io>
2020-01-20 15:34:09 +0100

    edit package

--- app/db.js
+++ app/db.js
@@ -1,5 +1,7 @@
 const sequelize = require('sequelize');
 
-const dbConnection = new sequelize.Sequelize(process.env.PG_URL);
+const dbConnection = new sequelize.Sequelize(process.env.PG_URL, {
+    dialect: 'postgres'
+});
 
 module.exports = dbConnection;
