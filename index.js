c8d67a9be99017d7555f3cdfaf311e9c74adf5e0
jpruliere <jean@oclock.io>
2020-01-23 17:16:35 +0100

    sécu + multer

--- index.js
+++ index.js
@@ -6,12 +6,21 @@ const app = express();
 
 const router = require('./app/router');
 
+const cors = require('cors');
+const multer = require('multer');
+const bodyParser = multer();
+
 const PORT = process.env.PORT || 3000;
+
+// pour contrôler qui peut contacter l'API
+app.use(cors('*'));
 
 // rend disponibles les données envoyées par l'utilisateur, via req.body
 app.use(express.urlencoded({
   extended: true
 }));
+
+app.use(bodyParser.none());
 
 app.use(router);
 
